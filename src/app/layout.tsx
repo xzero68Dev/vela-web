8241af32675745a07fe22baa461afaf941ffee1c
xzero68Dev <xzero.ng69@gmail.com>
2026-07-21 09:16:59 +0700

    feat: add Facebook Pixel 1799362394566251

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="th">
       <body>
+        {/* Google Analytics 4 */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-GMGH6GDS9N"
           strategy="afterInteractive"
@@ -46,6 +47,26 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           gtag('js', new Date());
           gtag('config', 'G-GMGH6GDS9N');
         `}</Script>
+
+        {/* Facebook Pixel */}
+        <Script id="fb-pixel" strategy="afterInteractive">{`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1799362394566251');
+          fbq('track', 'PageView');
+        `}</Script>
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=1799362394566251&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <AuthProvider>
           {children}
         </AuthProvider>
